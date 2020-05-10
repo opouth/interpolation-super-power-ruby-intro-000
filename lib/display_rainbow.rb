9db47980f05acd41e6fda385344e10fe2b1332b1
Ermite Rikudo <opouth@hotmail.fr>
2020-05-10 04:05:03 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -5,6 +5,7 @@ def display_rainbow(colors)
 end
 display_rainbow(colors)
 
-colors = "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
-
+def display_rainbow(colors)
+puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
+end
 display_rainbow(colors)
