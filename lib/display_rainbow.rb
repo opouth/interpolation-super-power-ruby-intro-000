b6897051540c364aa392f54bc6d2741db9596309
Ermite Rikudo <opouth@hotmail.fr>
2020-05-10 03:49:00 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -5,7 +5,7 @@ def display_rainbow(colors)
 end
 display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
 
-colors = "#{colors[0][0].capitalize}: #{colors[0]}, #{colors[1][0].capitalize}: #{colors[1]}, #{colors[2][0].capitalize}: #{colors[2]}, #{colors[3][0].capitalize}: #{colors[3]}, #{colors[4][0].capitalize}: #{colors[4]}, #{colors[5][0].capitalize}: #{colors[5]}, #{colors[6][0].capitalize}: #{colors[6]}"
-
+colors = "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
+ 
 
 display_rainbow(colors)
